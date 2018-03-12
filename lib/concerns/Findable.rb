09988c43b6b286f296254f262d755994bb61619e
Evan Candler <emcandler21@gmail.com>
2018-03-12 03:30:07 +0000

    automatically backed up by learn

--- lib/concerns/Findable.rb
+++ lib/concerns/Findable.rb
@@ -3,7 +3,7 @@ module Concerns::Findable
     create(name) || find_by_name(song_name)
   end
 
-  def self.find_by_name(song_name)
+  def find_by_name(song_name)
     self.all.detect {|song| song.name == song_name}
   end
 end
