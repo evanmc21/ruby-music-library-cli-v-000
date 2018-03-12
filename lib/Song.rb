f1f278357797389d8b8c8778b69dabe687853f0b
Evan Candler <emcandler21@gmail.com>
2018-03-12 03:56:20 +0000

    automatically backed up by learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -5,55 +5,57 @@ class Song
   @@all = []
 
   def initialize(name, artist = nil, genre = nil)
-      @name = name
-      self.artist = artist if artist
-      self.genre = genre if genre
-    end
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
 
-    def artist=(artist)
-      @artist = artist
-      artist.add_song(self)
-    end
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
 
-    def genre=(genre)
-      @genre = genre
-      genre.songs << self unless
-      genre.songs.include?(self)
-    end
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
 
   def self.all
     @@all
   end
 
-  def save
-    @@all << self
-  end
-
   def self.destroy_all
-    @@all.clear
+    all.clear
   end
 
-
-  def self.find_by_name(song_name)
-    all.detect {|song| song.name == song_name}
+  def save
+    self.class.all << self
   end
 
   def self.create(name)
     song = new(name)
     song.save
     song
+
+    # Or, as a one-liner:
+    # new(name).tap{ |s| s.save }
+  end
+
+  def self.find_by_name(name)
+    all.detect{ |s| s.name == name }
   end
 
   def self.find_or_create_by_name(name)
-    create(name) || find_by_name(song_name)
+    find_by_name(name) || create(name)
   end
 
   def self.new_from_filename(filename)
-    section = filename.split(" - ")
-    artist_name, song_name, genre_name = section[0], section[1], section[2].gsub(".mp3", "")
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
 
     artist = Artist.find_or_create_by_name(artist_name)
     genre = Genre.find_or_create_by_name(genre_name)
+
     new(song_name, artist, genre)
   end
 
