4e6a298e69f30c823a3c88edb98e4e0a05136c8f
Evan Candler <emcandler21@gmail.com>
2018-03-12 02:11:43 +0000

    automatically backed up by learn

---
+++ lib/Song.rb
@@ -0,0 +1,58 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist, :genre
+
+  @@all = []
+
+  def initialize(name, new_artist = nil, new_genre = nil)
+      @name = name
+    end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless
+    genre.songs.include?(self)
+  end
+
+  def self.find_by_name(song_name)
+    all.detect {|song| song.name == song_name}
+  end
+
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+  end
+
+  def self.find_or_create_by_name(name)
+    create(name) || find_by_name(song_name)
+  end
+
+  def self.new_from_filename(filename)
+    artist, song = filename.split(" - ")
+    new_tune = self.new(song)
+    new_tune.artist_name = artist
+    new_tune
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{ |s| s.save }
+  end
+end
---
+++ lib/music_importer.rb
@@ -0,0 +1,16 @@
+class MusicImporter
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files = Dir.entries(@path)
+    @files.delete_if{|file| file == "." || file == ".."}
+
+  end
+
+  def import
+    files.each {|filename| Song.new_by_filename(filename)}
+  end
+end
