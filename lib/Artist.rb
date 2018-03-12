a8a9cc97055ec94f310afe100d5a8c687edc9c90
Evan Candler <emcandler21@gmail.com>
2018-03-12 03:25:41 +0000

    automatically backed up by learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -7,37 +7,40 @@ class Artist
   @@all = []
 
   def initialize(name)
-      @name = name
-      @songs = []
-      @@all << self
-    end
+    @name = name
+    @@all << self
+    @songs = []
+  end
 
-    def self.all
-      @@all
-    end
+  def self.all
+    @@all
+  end
 
-    def save
-      @@all << self
-    end
+  def genres
+    @songs.collect{ |s| s.genre }.uniq
+  end
 
-    def self.destroy_all
-      @@all.clear
-    end
+  def self.destroy_all
+    @@all.clear
+  end
 
-    def self.create(name)
-      title = Artist.new(name)
-      title.save
-      title
-    end
+  def save
+    @@all << self
+  end
 
-    def add_song(song)
+  def save_song(song)
+    @songs << song
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+  end
+
+  def add_song(song)
       song.artist = self unless song.artist
-      songs << song unless song.include?(song)
-
+      save_song(song) unless songs.include?(song)
     end
 
-    def genres
-      @songs.collect{ |s| s.genre }.uniq
-    end
-end
+
+
 end
