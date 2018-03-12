e215b4a0dd987c09835568e68c3794429c7a59d4
Evan Candler <emcandler21@gmail.com>
2018-03-12 03:32:17 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -8,9 +8,7 @@ attr_accessor :path, :files
   end
 
   def files
-    @files = Dir.entries(@path)
-    @files.delete_if{|file| file == "." || file == ".."}
-
+  @files = Dir.glob("#{@path}/*.mp3").collect {|f| f.gsub("#{@path}/", "")}
   end
 
   def import
