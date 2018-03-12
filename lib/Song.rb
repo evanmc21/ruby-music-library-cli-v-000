e2a85ce3224ada622f4870c4a5023ba400028847
Evan Candler <emcandler21@gmail.com>
2018-03-12 03:38:16 +0000

    automatically backed up by learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -42,7 +42,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    create(name) || find_by_name(song_name)
+    find_by_name(song_name) || create(name)
   end
 
   def self.new_from_filename(filename)
