fe14a63aafd99321e6e68ae461d218e548a6f5ac
Evan Candler <emcandler21@gmail.com>
2018-03-12 03:55:09 +0000

    automatically backed up by learn

--- lib/Genre.rb
+++ lib/Genre.rb
@@ -39,42 +39,4 @@ class Genre
   def artists
     @songs.collect{ |s| s.artist }.uniq
   end
-end  extend Concerns::Findable
-  attr_accessor :name
-
-  @@all = []
-
-  def initialize(name)
-      @name = name
-      @songs = []
-      @@all << self
-    end
-
-  def self.all
-    @@all
-  end
-
-  def save
-    @@all << self
-  end
-
-  def self.destroy_all
-    @@all.clear
-  end
-
-  def self.create(name)
-    title = Genre.new(name)
-    title.save
-    title
-  end
-
-  def artist
-    songs.map { |s| s.artist }.uniq
-  end
-
-  def add_song(song)
-    if !self.songs.include?(song)
-      @songs << song
-    end
-  end
 end
